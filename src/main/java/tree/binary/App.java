c7790ce1ff1e408dc9c2052c1734d5ee6aacb76c
marceli10 <kunajkuuu@gmail.com>
2022-05-03 20:36:17 +0200

    created project structure

---
+++ src/main/java/tree/binary/App.java
@@ -0,0 +1,13 @@
+package tree.binary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
