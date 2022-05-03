8ded071aa67d5c8245e6ba7e78591c455aaa6b8d
marceli10 <kunajkuuu@gmail.com>
2022-05-03 23:17:47 +0200

    cleaned imports and added basic App example

--- src/main/java/tree/binary/App.java
+++ src/main/java/tree/binary/App.java
@@ -1,13 +1,40 @@
 package tree.binary;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import tree.binary.data.structure.BinaryTree;
+import tree.binary.data.structure.IBinaryTree;
+import tree.binary.data.structure.INode;
+import tree.binary.data.structure.Node;
+
+public class App {
+    public static void main(String[] args) {
+        INode node1 = new Node(20);
+        INode node2 = new Node(30);
+        INode node3 = new Node(40);
+        INode node4 = new Node(50);
+        INode node5 = new Node(60);
+
+        IBinaryTree tree = new BinaryTree(node1);
+        tree.addRightNode(node1, node2);
+        tree.addRightNode(node2, node3);
+        tree.addLeftNode(node2, node4);
+        tree.addLeftNode(node3, node5);
+
+        System.out.printf("[*] Leafs count: %d\n", tree.leafsCount());
+        System.out.printf("[*] Largest number of edges: %d\n", tree.largestNumberOfEdges());
+
+        INode nodeA = new Node(65);
+        INode nodeB = new Node(66);
+        INode nodeC = new Node(67);
+        INode nodeD = new Node(68);
+        INode nodeE = new Node(69);
+        IBinaryTree tree2 = new BinaryTree(nodeA);
+
+        tree2.addRightNode(nodeA, nodeB);
+        tree2.addLeftNode(nodeA, nodeD);
+        tree2.addLeftNode(nodeD, nodeC);
+        tree2.addLeftNode(nodeC, nodeE);
+
+        System.out.printf("[*] tree == tree2: %b\n", tree.equals(tree2));
+
     }
 }
--- src/test/java/tree/binary/data/structure/BinaryTreeTest.java
+++ src/test/java/tree/binary/data/structure/BinaryTreeTest.java
@@ -1,10 +1,9 @@
 package tree.binary.data.structure;
 
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class BinaryTreeTest {
 
